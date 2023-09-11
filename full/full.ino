3ad61ca515458564562985fc6513f3a9de034f3c
fadhillaxl <99535995+fadhillaxl@users.noreply.github.com>
2023-09-11 17:19:20 +0700

    add full

--- full/full.ino
+++ full/full.ino
@@ -1,56 +1,37 @@
 #include <Stepper.h>
-#include "Wire.h"
+#include <Wire.h>
 #include <MPU6050_light.h>
 #include <Kalman.h>
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 #include <Sgp4.h>
 
+const int limEU = 13;
+const int limED = 12;
+const int limAZ = 14;
+const unsigned long GPSPeriod = 3000;
 
 Sgp4 sat;
-unsigned long unixtime = 1458950400;
-int timezone = 7 ;  //utc + 12
-
 SoftwareSerial serial_gps(17, 16);
 TinyGPSPlus gps;
 unsigned long GPSStartMillis;
 unsigned long currentMillis;
-const unsigned long GPSPeriod = 3000;   // Periode GPS
 
 float Longitude = 0;
 float Latitude = 0;
 float Speed = 0;
 float Altitude = 0;
 
-int AZ,EL;
-
-Kalman kalmanX;
 Kalman kalmanY;
-
 const int stepsPerRevolution = 200 * 4;
-const int dir = 13;
-const int puly = 4;
-const int diry = 2;
-const int pulx = 19;
-const int dirx = 18;
-const int limEU = 13;
-const int limED = 12;
-const int limAZ = 14;
-
+Stepper myStepper(stepsPerRevolution, 4, 2);
+Stepper myStepperx(stepsPerRevolution, 18, 19);
 MPU6050 mpu(Wire);
-
-Stepper myStepper(stepsPerRevolution, puly, diry);
-Stepper myStepperX(stepsPerRevolution, pulx, dirx);
-const int delayMS = 0;
 
 void kalibrasi() {
   while (digitalRead(limEU) != HIGH) {
-    if (digitalRead(limED) != HIGH) {
-      myStepper.step(1);
-    }
-    else {
-      myStepper.step(-1);
-    }
+    int limEDStatus = digitalRead(limED);
+    myStepper.step(limEDStatus != HIGH ? 1 : -1);
   }
   Serial.println("Kalibrasi Selesai!\n");
 }
@@ -68,11 +49,11 @@ void FunctionGPS() {
       Longitude = gps.location.lng();
       Speed = gps.speed.kmph();
       Altitude = gps.altitude.meters();
-      
+
       Serial.println("");
       Serial.println("");
       Serial.println("Memeriksa Ulang Sinyal GPS");
-      
+
       Serial.print("Latitude= ");
       Serial.println(Latitude, 6);
       Serial.print("Longitude= ");
@@ -92,7 +73,14 @@ void setgps() {
   GPSStartMillis = millis();
 }
 
+void sgp4(){
+  sat.site(Latitude, Longitude, Altitude);
+  char satname[] = "ISS (ZARYA)";
+  char tle_line1[] = "1 25544U 98067A   16065.25775256 -.00164574  00000-0 -25195-2 0  9990";  //Line one from the TLE data
+  char tle_line2[] = "2 25544  51.6436 216.3171 0002750 185.0333 238.0864 15.54246933988812";  //Line two from the TLE data
+  sat.init(satname, tle_line1, tle_line2);
 
+}
 
 void setup() {
   pinMode(limEU, OUTPUT);
@@ -102,42 +90,42 @@ void setup() {
   kalibrasi();
   Wire.begin();
   byte status = mpu.begin();
-  
+
   Serial.print(F("Status MPU6050: "));
   Serial.println(status);
-  
+
   while (status != 0) { }
-  
+
   Serial.println(F("Menghitung Offset, jangan gerakkan MPU6050"));
   delay(10000);
   mpu.calcOffsets(true, true);
   Serial.println("Selesai!\n");
   myStepper.setSpeed(180);
-  kalmanX.setAngle(0);
   kalmanY.setAngle(0);
   setgps();
-  sat.site(Latitude,Longitude,Altitude); //set site latitude[°], longitude[°] and altitude[m]
-  char satname[] = "ISS (ZARYA)";
-  char tle_line1[] = "1 25544U 98067A   16065.25775256 -.00164574  00000-0 -25195-2 0  9990";  //Line one from the TLE data
-  char tle_line2[] = "2 25544  51.6436 216.3171 0002750 185.0333 238.0864 15.54246933988812";  //Line two from the TLE data
-  
-  sat.init(satname,tle_line1,tle_line2);     //initialize satellite parameters 
-  AZ = sat.satAz;
-  EL = sat.satEl;
+  sgp4();
 
 }
 
 void loop() {
   mpu.update();
-  double kalman_Y = kalmanY.getAngle(mpu.getAngleY(), mpu.getAccAngleY() / 131.0, 0.01);
+  double kalman_Y = kalmanY.getAngle(mpu.getAngleY(), mpu.getAccAngleY() / 131.0, 0.01);s
   double Y = map(kalman_Y * 100, 0, 9000, 9000, 0);
+  double kalman_X = kalmanY.getAngle(mpu.getAngleX(), mpu.getAccAngleX() / 131.0, 0.01);
   Serial.println(Y);
 
-  if (int(Y) > EL) {
+  if (int(Y) > sat.satEl) {
     myStepper.step(17);
   }
-  else if (int(Y) < EL) {
+  else if (int(Y) < sat.satEl) {
     myStepper.step(-17);
+  }
+
+  if (int(kalman_X) > sat.satAz) {
+    myStepperx.step(17);
+  }
+  else if (int(kalman_X) < sat.satAz) {
+    myStepperx.step(-17);
   }
   delay(10);
 }
