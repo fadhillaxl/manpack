a6edd1b24e2214975c0951f2ece76ef1447d15e4
ITSINNAR <99535995+fadhillaxl@users.noreply.github.com>
2023-09-11 15:27:43 +0700

    fixed kalibrasi

--- stabilizer_azimuth/stabilizer_azimuth.ino
+++ stabilizer_azimuth/stabilizer_azimuth.ino
@@ -9,18 +9,18 @@ SoftwareSerial serial_gps(17, 16);
 TinyGPSPlus gps;
 unsigned long GPSStartMillis;
 unsigned long currentMillis;
-const unsigned long GPSPeriod = 3000;   //GPS period
+const unsigned long GPSPeriod = 3000;   // Periode GPS
 
 float Longitude = 0;
 float Latitude = 0;
 float Speed = 0;
 float Altitude = 0;
 
-
 Kalman kalmanX;
 Kalman kalmanY;
-const int stepsPerRevolution = 200*4;  // change this to fit the number of steps per revolution
-int dir = 13;
+
+const int stepsPerRevolution = 200 * 4;
+const int dir = 13;
 const int puly = 4;
 const int diry = 2;
 const int pulx = 19;
@@ -28,27 +28,23 @@ const int dirx = 18;
 const int limEU = 13;
 const int limED = 12;
 const int limAZ = 14;
+
 MPU6050 mpu(Wire);
 
 Stepper myStepper(stepsPerRevolution, puly, diry);
 Stepper myStepperX(stepsPerRevolution, pulx, dirx);
 const int delayMS = 0;
 
-
-void kalibrasi(){
-  while (limEU != HIGH)
-  {
-    if(limED != HIGH){
+void kalibrasi() {
+  while (digitalRead(limEU) != HIGH) {
+    if (digitalRead(limED) != HIGH) {
       myStepper.step(1);
-
-    }else{
+    }
+    else {
       myStepper.step(-1);
-
     }
   }
-
-  Serial.println("Kalibrasi Done!\n");
-  
+  Serial.println("Kalibrasi Selesai!\n");
 }
 
 void CheckGPS() {
@@ -58,80 +54,71 @@ void CheckGPS() {
 }
 
 void FunctionGPS() {
-  if (currentMillis - GPSStartMillis >= GPSPeriod)  //test whether the period has elapsed
-  {
+  if (currentMillis - GPSStartMillis >= GPSPeriod) {
     if (gps.location.isUpdated()) {
       Latitude = gps.location.lat();
       Longitude = gps.location.lng();
       Speed = gps.speed.kmph();
       Altitude = gps.altitude.meters();
+      
       Serial.println("");
       Serial.println("");
-
-      Serial.println("Re-Checking GPS Signal");
+      Serial.println("Memeriksa Ulang Sinyal GPS");
+      
       Serial.print("Latitude= ");
       Serial.println(Latitude, 6);
-
       Serial.print("Longitude= ");
       Serial.println(Longitude, 6);
-
-      Serial.print("Altitude(meters)= ");
+      Serial.print("Altitude (meter)= ");
       Serial.println(Altitude, 2);
-
-      Serial.print("SPEED(KMPH)= ");
+      Serial.print("Kecepatan (KMPH)= ");
       Serial.println(Speed, 2);
     }
-    GPSStartMillis = currentMillis;  //IMPORTANT to save the start time of the current LED state.
+    GPSStartMillis = currentMillis;
   }
 }
 
-void setgps(){
+void setgps() {
   serial_gps.begin(9600);
-  Serial.println("GPS Mulai");
-  GPSStartMillis = millis();    //initial start time
-
+  Serial.println("GPS Dimulai");
+  GPSStartMillis = millis();
 }
 
 void setup() {
-  // set the speed at 60 rpm:
-  pinMode(limEU,OUTPUT);
-  pinMode(limED,OUTPUT);
-  pinMode(limAZ,OUTPUT);
+  pinMode(limEU, OUTPUT);
+  pinMode(limED, OUTPUT);
+  pinMode(limAZ, OUTPUT);
   Serial.begin(9600);
   kalibrasi();
   Wire.begin();
-  
   byte status = mpu.begin();
-  Serial.print(F("MPU6050 status: "));
-  Serial.println(status);
-  while(status!=0){ } // stop everything if could not connect to MPU6050
   
-  Serial.println(F("Calculating offsets, do not move MPU6050"));
+  Serial.print(F("Status MPU6050: "));
+  Serial.println(status);
+  
+  while (status != 0) { }
+  
+  Serial.println(F("Menghitung Offset, jangan gerakkan MPU6050"));
   delay(10000);
-  mpu.calcOffsets(true,true); // gyro and accelero
-  Serial.println("Done!\n");
+  mpu.calcOffsets(true, true);
+  Serial.println("Selesai!\n");
   myStepper.setSpeed(180);
   kalmanX.setAngle(0);
   kalmanY.setAngle(0);
-
   setgps();
-
-
 }
 
 void loop() {
-  // step one revolution in one direction:
   mpu.update();
-  double kalman_Y = kalmanY.getAngle(mpu.getAngleY(),mpu.getAccAngleY()/131.0,0.01);
-  double Y = map(kalman_Y*100,0,9000,9000,0);
-  // Serial.print("angle : ");
+  double kalman_Y = kalmanY.getAngle(mpu.getAngleY(), mpu.getAccAngleY() / 131.0, 0.01);
+  double Y = map(kalman_Y * 100, 0, 9000, 9000, 0);
   Serial.println(Y);
-  // Serial.println(mpu.getAngleY());
-  if(int(Y) > 82){
+
+  if (int(Y) > 82) {
     myStepper.step(17);
-  }else if(int(Y) < 82){
+  }
+  else if (int(Y) < 82) {
     myStepper.step(-17);
   }
   delay(10);
-
 }
